3caefcc0a71d9988cda675fe7943c7d3d6c42ef3
Eduardo Alencar <alencareduardo17@gmail.com>
2023-10-27 12:46:07 -0300

    Implementando a animação de galeria imagens em JavaScript

--- js/script.js
+++ js/script.js
@@ -45,3 +45,24 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+
+// Galeria de bicicleta
+
+const galeria = document.querySelectorAll(".bicicletas-imagens img");
+const galeriaContainer = document.querySelector(".bicicletas-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    console.log(media);
+    if(media) {
+        galeriaContainer.prepend(img); 
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
